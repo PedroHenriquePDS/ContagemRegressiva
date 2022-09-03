e6a48afe85100f0c54a7d14fcb71a53d0f49a74d
Pedro Henrique <pedrohenrique053010@gmail.com>
2022-09-02 22:42:13 -0300

    resolução do exercicio

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Preparar...");
+Console.ReadKey();
+Console.WriteLine("Apontar...");
+Console.ReadKey();
+Console.WriteLine("FOGO!");
+Console.WriteLine("Pressione uma tecla para fechar...");
+Console.ReadKey();
